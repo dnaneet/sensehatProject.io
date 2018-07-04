8fe4bffc4f8b4e61cfbaaffb4b37d1a64bc2f472
Aneet D Narendranath <dnaneet@mtu.edu>
2018-07-04 13:28:17 -0400

    Add files via upload

--- process_data.sh
+++ process_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Run python script to collect and store temperature and rel humidity data
-/usr/bin/python /home/pi/Desktop/example00.py
+/usr/bin/python /home/pi/Desktop/example02.py
 #awk '{p=f;f=$1} NR>1{print f-p}' /home/pi/Desktop/temperature.txt >> dT.txt
 #awk '{p=f;f=$1} NR>1{print f-p}' /home/pi/Desktop/rh.txt >> dr.txt
 #Plot data
@@ -16,6 +16,12 @@
 
 #/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/rhws.png /sensehat
 
-/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/*.png /sensehat
+/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/$(ls -ltrh T*.pdf | tail -n 1 | awk '{print $9}') /sensehat
 
-rm /home/pi/Desktop/*.png
+/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/$(ls -ltrh rh*.pdf | tail -n 1 | awk '{print $9}') /sensehat
+
+/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/$(date -d "yesterday 13:00" '+%y-%m-%d').temperature.txt /sensehat/data
+/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/$(date -d "yesterday 13:00" '+%y-%m-%d').rh.txt /sensehat/data
+
+
+#rm /home/pi/Desktop/*.png
