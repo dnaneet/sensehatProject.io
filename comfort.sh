e4952971c91f6064071ef8ee73ea37b0ab8c257b
Aneet D Narendranath <dnaneet@mtu.edu>
2018-08-18 08:13:07 -0400

    Newest version of gnuplot script.

---
+++ comfort.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/gnuplot -persist
+#set terminal postscript landscape noenhanced monochrome \
+#              dashed defaultplex "Helvetica" 14
+#set terminal pdf size 400,300 enhanced font "Helvetica,20"
+set terminal postscript defaultplex "Helvetica" 20
+date = system("date +%F")
+#set output '| ps2pdf - comfort.pdf'
+set output '| ps2pdf - /home/pi/Desktop/comfort.'.date.'.pdf'
+
+set multiplot layout 2, 1 title "Comfort by the Harp"
+set nokey
+set xlabel "time"
+set ylabel "RH"
+set xrange [ 0 : 287 ] noreverse nowriteback
+set yrange [ 45 : 70 ] noreverse nowriteback
+plot "/home/pi/Desktop/rh.txt" using 1
+
+set nokey
+set xlabel "time"
+set ylabel "temperature"
+set xrange [ 0 : 287 ]
+set yrange [ 20 : 30 ]
+plot "/home/pi/Desktop/temperature.txt" using 1
+
+#eof
