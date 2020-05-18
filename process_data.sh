1fb4efa5b559142ac11567e8a75ca7c606a2b4db
dnaneet@mtu.edu <dnaneet@mtu.edu>
2020-05-18 17:05:42 -0400

    data

--- process_data.sh
+++ process_data.sh
@@ -8,7 +8,8 @@
 cp /home/pi/Desktop/sensehatProject/data/data.csv /home/pi/Desktop/sensehatProject/data/$(date -d "today" '+%y-%m-%d').data.csv
 /usr/bin/git pull -q origin master
 /usr/bin/git add --all 
-/usr/bin/git commit -m "data"
+/usr/bin/git commit -m "data" &
+sleep 5
 /usr/bin/git push -q origin master
 
 
