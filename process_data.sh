df81fb73ba1d85c43e82a7beeaf405c55307c2fc
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2020-05-18 14:59:32 -0400

    Add files via upload

--- process_data.sh
+++ process_data.sh
@@ -2,24 +2,19 @@
 
 #''Run python script to collect and store temperature and rel humidity data''#
 /usr/bin/python /home/pi/Desktop/example02.py
-
-#awk '{p=f;f=$1} NR>1{print f-p}' /home/pi/Desktop/temperature.txt >> dT.txt
-#awk '{p=f;f=$1} NR>1{print f-p}' /home/pi/Desktop/rh.txt >> dr.txt
-#Plot data
-#/usr/bin/gnuplot /home/pi/Desktop/piplot.sh
-#/home/pi/Desktop/./process_data_ws.sh
+/usr/bin/python /home/pi/Desktop/sense_therm.py  #produces data.csv
+#/usr/bin/python /home/pi/Desktop/acceleration.py
+#/usr/bin/python /home/pi/Desktop/mag.py >> /home/pi/Desktop/mag.txt
+cp /home/pi/Desktop/data.csv /home/pi/Desktop/$(date -d "today" '+%y-%m-%d').data.csv
 
 #''Plotting''#
 /home/pi/Desktop/./comfort.sh #Gnuplot to plot the collected data, 2018-Aug-18
 
 #''Upload data to dropbox''#
-#Upload data to dropbox
 /home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/rh.txt /sensehat 
 /home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/temperature.txt /sensehat
+/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/$(date -d "today" '+%y-%m-%d').data.csv /sensehat
 
-
-#/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload $(ls -ltrh /home/pi/Desktop/rh*.pdf  | tail -n 1 | awk '{print $9}') /sensehat
-#/home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload $(ls -ltrh /home/pi/Desktop/T*.pdf  | tail -n 1 | awk '{print $9}') /sensehat
 
 #''Upload plot to dropbox''#
 /home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload $(ls -ltrh /home/pi/Desktop/comfort*.pdf  | tail -n 1 | awk '{print $9}') /sensehat
@@ -28,4 +23,3 @@
 /home/pi/Dropbox-Uploader/./dropbox_uploader.sh upload /home/pi/Desktop/$(date -d "yesterday 13:00" '+%y-%m-%d').rh.txt /sensehat/data
 
 
-#rm /home/pi/Desktop/*.png
---
+++ sense_therm.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+from sense_hat import SenseHat
+import io
+import time
+import csv
+import numpy as np
+import pandas as pd
+
+sense = SenseHat()
+sense.clear()
+sense.set_rotation(90)
+
+#f = open("/home/pi/Desktop/temperature.txt", "a+") #Temperature
+#g = open("/home/pi/Desktop/rh.txt", "a+") #relative humidity
+
+#loop to record every n seconds
+n=9 #4 readings to go with the first reading of temperature and relative humidity
+temp=np.array(sense.get_temperature()) #first reading, temperature
+rh=np.array(sense.get_humidity()) #first reading, relative humidity
+#g = np.array(sense.get_accelerometer_raw()) #acceleration vector
+for i in range(n):
+	temp = np.append(temp, sense.get_temperature())
+	rh = np.append(rh, sense.get_humidity())
+#	compass=sense.get_compass_raw()
+#	x = float(compass['z'])
+#	y = float(compass['y'])
+#	z = float(compass['x'])
+	time.sleep(0.05)
+
+time_stamp = str(pd.datetime.now())
+df = pd.DataFrame({'timestamp': time_stamp, 'temperature': np.mean(temp), 'relativeHumidity': np.mean(rh)}, index=[0])
+df.to_csv('/home/pi/Desktop/data.csv', mode='a', header=False)
+
+#print temp[1:] #Print for debugging
+#print np.mean(temp[1:]) #Print for debugging
+
+#f.write('%s\n' % np.mean(temp)) #Average value of temperature for 5 consecutive readings
+#g.write('%s\n' % np.mean(rh))   #Average value of tempeature for 5 consecutive readings
+
+#f.close()
+#g.close()
+
+##Code below may be used to measure cpu temperature
+#f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+#f = open("/usr/bin/vcgencmd measure_temp", "r")
+#t = f.readline ()
+#cputemp = "CPU temp: "+t
+#print (cputemp)
+
+#eof
