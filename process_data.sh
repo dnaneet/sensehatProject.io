fe1291c332901c0902ed2252b5a8f37d03ec6d74
dnaneet@mtu.edu <dnaneet@mtu.edu>
2020-05-18 16:00:30 -0400

    data

--- process_data.sh
+++ process_data.sh
@@ -6,6 +6,10 @@
 #/usr/bin/python /home/pi/Desktop/acceleration.py
 #/usr/bin/python /home/pi/Desktop/mag.py >> /home/pi/Desktop/mag.txt
 cp /home/pi/Desktop/sensehatProject/data/data.csv /home/pi/Desktop/sensehatProject/data/$(date -d "today" '+%y-%m-%d').data.csv
+/usr/bin/git add /home/pi/Desktop/sensehatProject
+/usr/bin/git commit -m "data"
+/usr/bin/git push origin master
+
 
 #''Plotting''#
 #/home/pi/Desktop/./comfort.sh #Gnuplot to plot the collected data, 2018-Aug-18
