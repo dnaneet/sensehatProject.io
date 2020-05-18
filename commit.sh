fd947a81a0927b6f600f5570cbdeaafaf316124b
dnaneet@mtu.edu <dnaneet@mtu.edu>
2020-05-18 19:45:01 -0400

    regular commit

--- commit.sh
+++ commit.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd /home/pi/Desktop/sensehatProject
-/usr/bin/git add --all && /usr/bin/git commit -m "regular commit"
-cd /home/pi/Desktop/sensehatProject
-/usr/bin/git push origin master 
+cd /home/pi/Desktop/sensehatProject && /usr/bin/git add --all && /usr/bin/git commit -m "regular commit"
+cd /home/pi/Desktop/sensehatProject && /usr/bin/git push origin master 
